ccb5808eb5e7c813c2d5867e28ff623fdce20abc
GoldenToro <max.goeldnernew@gmail.com>
2024-06-14 11:30:46 +0200

    created logs

---
+++ load_data_sets.py
@@ -0,0 +1,17 @@
+from fancy_logging import create_logger
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+
+    logger = create_logger("load_data_sets")
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
